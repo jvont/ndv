ad87d4123540301a9f6da537ed6d61289ac7ea53
Johann von Tiesenhausen <blah424@gmail.com>
2021-09-06 18:59:54 -0400

    added quat header

--- src/ndv/geometric.h
+++ src/ndv/geometric.h
@@ -1,4 +1,4 @@
 // reflect, refract, normalize, distance, vector length, perpendicular, cross, 
 // transpose, inverse
 // angles (between vectors, etc.)
-// TODO: separate into mat_geometric, vec_geometric, and quat_geometric files
+// TODO: separate into mat_geometric, vec_geometric, and quat_geometric files 
