dc13acfcb08b780a79c4f18af8a4af8151395007
Johann von Tiesenhausen <blah424@gmail.com>
2021-09-06 15:16:52 -0400

    added initial mat struct (last commit)

--- main.cpp
+++ main.cpp
@@ -6,16 +6,9 @@ using namespace bones;
 
 int main()
 {
-  // Vec2f v1({1.2f, 3.1f});
-  // Vec2f v2({2.2f, 1.1f});
 
-  // Vec2f v = v1 + v2;
-
-  // Mat<2, 2, float> m({{0, 1},{2, 3}});
-
-  Mat<2, 2, float> m({0, 1, 2, 3});
-
-  Vec<2, float> v = Vec2f::zero;
+  Mat<2, 2, float> m1( {0, 1, 2, 3} );
+  Mat<2, 2, float> m2( {{0, 1}, {2, 3}} );
 
   return 0;
 }
