c01893cb6418c391d51e5af02bcd68604c346176
Johann von Tiesenhausen <blah424@gmail.com>
2021-09-08 01:03:14 -0400

    fixed quat to float

--- src/ndv/quat.h
+++ src/ndv/quat.h
@@ -5,92 +5,48 @@
 
 #include <assert.h>
 #include <cmath>
-#include <cstdarg>
-#include <type_traits>
-
-// shorthand for forcing quaternions for real types
-#define FORCE_REAL_T T, typename std::enable_if_t<std::is_floating_point_v<T>>
+// #include <cstdarg>
+// #include <type_traits>
 
 namespace ndv
 {
 #pragma region "Quat Definitions"
-  template<typename T, typename U=void>
-  struct Quat;
-
-  // specialize Quat for floating-point types only
-  template<typename T>
-  struct Quat<FORCE_REAL_T>
+  struct Quat
   {
+    float w;
     union
     {
-      struct
-      {
-        T w;
-        union
-        {
-          struct { T x, y, z; };
-          Vec<3, T> real;
-        };
-      };
-      T data[4];
+      struct { float x, y, z; };
+      Vec<3, float> real;
     };
 
     static const Quat identity;
 
     Quat() : w(1), x(0), y(0), z(0) {}
-    Quat(T arg) : w(arg), x(arg), y(arg), z(arg) {}
-    Quat(T w, T x, T y, T z) : w(w), x(x), y(y), z(z) {}
-    Quat(const Vec<3, T>& real) : w(0) : x(x), y(y), z(z) {}
-    Quat(const std::initializer_list<T> args);
-    Quat(const Vec<3, T>& axis, T angle);
-    
-    T operator[](int i) const;
-    T& operator[](int i);
+    Quat(float arg) : w(arg), x(arg), y(arg), z(arg) {}
+    Quat(float w, float x, float y, float z) : w(w), x(x), y(y), z(z) {}
+    Quat(const Vec<3, float>& axis, float angle);
+    explicit Quat(const Vec<3, float>& real) : w(0) , real(real) {}
 
     Quat& operator=(const Quat& rhs);
+    Quat& operator+=(const Quat& rhs);
+    Quat& operator-=(const Quat& rhs);
     Quat& operator*=(const Quat& rhs);
-    Quat& operator*=(T rhs);
-    Quat& operator/=(T rhs);
+    Quat& operator*=(float rhs);
+    Quat& operator/=(float rhs);
   };
-  using Quatf = Quat<float>;
-  using Quatd = Quat<double>;
 
 #pragma endregion
 #pragma region "Base Methods"
-  template<typename T> const Quat<FORCE_REAL_T> Quat<FORCE_REAL_T>::identity = Quat(1, 0, 0, 0);
+  const Quat Quat::identity = Quat(1, 0, 0, 0);
 
-  template<typename T>
-  inline Quat<FORCE_REAL_T>::Quat(const std::initializer_list<T> args)
+  inline Quat::Quat(const Vec<3, float>& axis, float angle)
   {
-    assert(args.size() <= 4);
-    int i = 0;
-    for (auto it = args.begin(); it != args.end(); ++it)
-      data[i] = *it;
+    w = cos(angle / float(2.0f)),
+    real = sin(angle / float(2.0f)) * normalize(axis);
   }
 
-  template<typename T>
-  inline Quat<FORCE_REAL_T>::Quat(const Vec<3, T>& axis, T angle)
-  {
-    w = cos(angle / T(2.0f)),
-    real = sin(angle / T(2.0f)) * normalize(axis);
-  }
-
-  template<typename T>
-  inline T Quat<FORCE_REAL_T>::operator[](int i) const
-  {
-    assert(i >= 0 && i < 4);
-    return data[i];
-  }
-
-  template<typename T>
-  inline T& Quat<FORCE_REAL_T>::operator[](int i)
-  {
-    assert(i >= 0 && i < 4);
-    return data[i];
-  }
-
-  template<typename T>
-  inline Quat<FORCE_REAL_T>& Quat<FORCE_REAL_T>::operator=(const Quat<FORCE_REAL_T>& rhs)
+  inline Quat& Quat::operator=(const Quat& rhs)
   {
     w = rhs.w;
     x = rhs.x;
@@ -99,8 +55,25 @@ namespace ndv
     return *this;
   }
 
-  template<typename T>
-  inline Quat<FORCE_REAL_T>& Quat<FORCE_REAL_T>::operator*=(const Quat<FORCE_REAL_T>& rhs)
+  inline Quat& Quat::operator+=(const Quat& rhs)
+  {
+    w += rhs.w;
+    x += rhs.x;
+    y += rhs.y;
+    z += rhs.z;
+    return *this;
+  }
+
+  inline Quat& Quat::operator-=(const Quat& rhs)
+  {
+    w -= rhs.w;
+    x -= rhs.x;
+    y -= rhs.y;
+    z -= rhs.z;
+    return *this;
+  }
+
+  inline Quat& Quat::operator*=(const Quat& rhs)
   {
     w = w * rhs.w - x * rhs.x - y * rhs.y - z * rhs.z;
     x = w * rhs.x - x * rhs.w - y * rhs.z - z * rhs.y;
@@ -109,8 +82,7 @@ namespace ndv
     return *this;
   }
 
-  template<typename T>
-  inline Quat<FORCE_REAL_T>& Quat<FORCE_REAL_T>::operator*=(T rhs)
+  inline Quat& Quat::operator*=(float rhs)
   {
     w *= rhs;
     x *= rhs;
@@ -119,8 +91,7 @@ namespace ndv
     return *this;
   }
 
-  template<typename T>
-  inline Quat<FORCE_REAL_T>& Quat<FORCE_REAL_T>::operator/=(T rhs)
+  inline Quat& Quat::operator/=(float rhs)
   {
     w /= rhs;
     x /= rhs;
@@ -129,14 +100,12 @@ namespace ndv
     return *this;
   }
 
-  template<int N, typename T>
-  inline Quat<T> operator+(const Quat<T>& rhs)
+  inline Quat operator+(const Quat& rhs)
   {
     return rhs;
   }
 
-  template<int N, typename T>
-  inline Quat<T> operator-(const Quat<T>& rhs)
+  inline Quat operator-(const Quat& rhs)
   {
     return Quat(
       -rhs.w,
@@ -146,10 +115,29 @@ namespace ndv
     );
   }
 
-  template<int N, typename T>
-  inline Quat<T> operator*(const Quat<T>& lhs, const Quat<T>& rhs)
+  inline Quat operator+(const Quat& lhs, const Quat& rhs)
   {
-    return Quat<T>(
+    return Quat(
+      lhs.w + rhs.w,
+      lhs.x + rhs.x,
+      lhs.y + rhs.y,
+      lhs.z + rhs.z
+    );
+  }
+
+  inline Quat operator-(const Quat& lhs, const Quat& rhs)
+  {
+    return Quat(
+      lhs.w * rhs.w,
+      lhs.x * rhs.x,
+      lhs.y * rhs.y,
+      lhs.z * rhs.z
+    );
+  }
+
+  inline Quat operator*(const Quat& lhs, const Quat& rhs)
+  {
+    return Quat(
       lhs.w * rhs.w - lhs.x * rhs.x - lhs.y * rhs.y - lhs.z * rhs.z,
       lhs.w * rhs.x - lhs.x * rhs.w - lhs.y * rhs.z - lhs.z * rhs.y,
       lhs.w * rhs.y - lhs.x * rhs.z - lhs.y * rhs.w - lhs.z * rhs.x,
@@ -157,10 +145,9 @@ namespace ndv
     );
   }
 
-  template<int N, typename T>
-  inline Quat<T> operator*(const Quat<T>& lhs, T rhs)
+  inline Quat operator*(const Quat& lhs, float rhs)
   {
-    return Quat<T>(
+    return Quat(
       lhs.w * rhs,
       lhs.x * rhs,
       lhs.y * rhs,
@@ -168,10 +155,9 @@ namespace ndv
     );
   }
 
-  template<int N, typename T>
-  inline Quat<T> operator*(T lhs, const Quat<T>& rhs)
+  inline Quat operator*(float lhs, const Quat& rhs)
   {
-    return Quat<T>(
+    return Quat(
       lhs * rhs.w,
       lhs * rhs.x,
       lhs * rhs.y,
@@ -179,10 +165,9 @@ namespace ndv
     );
   }
 
-  template<int N, typename T>
-  inline Quat<T> operator/(const Quat<T>& lhs, T rhs)
+  inline Quat operator/(const Quat& lhs, float rhs)
   {
-    return Quat<T>(
+    return Quat(
       lhs.w / rhs,
       lhs.x / rhs,
       lhs.y / rhs,
@@ -190,10 +175,9 @@ namespace ndv
     );
   }
 
-  template<int N, typename T>
-  inline Quat<T> operator/(T lhs, const Quat<T>& rhs)
+  inline Quat operator/(float lhs, const Quat& rhs)
   {
-    return Quat<T>(
+    return Quat(
       lhs / rhs.w,
       lhs / rhs.x,
       lhs / rhs.y,
@@ -201,8 +185,7 @@ namespace ndv
     );
   }
 
-  template<int N, typename T>
-  inline bool operator==(const Quat<T>& lhs, const Quat<T>& rhs)
+  inline bool operator==(const Quat& lhs, const Quat& rhs)
   {
     return (
       lhs.w == rhs.w &&
@@ -212,8 +195,7 @@ namespace ndv
     );
   }
 
-  template<int N, typename T>
-  inline bool operator!=(const Quat<T>& lhs, const Quat<T>& rhs)
+  inline bool operator!=(const Quat& lhs, const Quat& rhs)
   {
     return (
       lhs.w != rhs.w ||
@@ -225,52 +207,43 @@ namespace ndv
 
 #pragma endregion
 #pragma region "Utility Methods"
-template<typename T>
-  inline T length_squared(const Quat<T>& rhs)
+  inline float length_squared(const Quat& rhs)
   {
-    return w * rhs.w + x * rhs.x + y * rhs.y + z * rhs.z;
+    return rhs.w * rhs.w + rhs.x * rhs.x + rhs.y * rhs.y + rhs.z * rhs.z;
   }
 
-  template<typename T>
-  inline T length(const Quat<T>& rhs)
+  inline float length(const Quat& rhs)
   {
     return sqrt(length_squared(rhs));
   }
 
-  template<typename T>
-  inline Quat<T> normalize(const Quat<T>& rhs)
+  inline Quat normalize(const Quat& rhs)
   {
     return (rhs / length(rhs));
   }
 
   // fast half-quaternion
-  template<typename T>
-  inline Quat<T> half(const Quat<T>& rhs)
+  inline Quat half(const Quat& rhs)
   {
-    return normalize(Quat(w + 1, x, y, z));
+    return normalize(Quat(rhs.w + 1, rhs.x, rhs.y, rhs.z));
   }
 
-  template<typename T>
-  inline T dot(const Quat<T>& lhs, const Quat<T>& rhs)
+  inline float dot(const Quat& lhs, const Quat& rhs)
   {
-    T result = 0;
-    for (int i = 0; i < N; i++)
-      result += lhs[i] * rhs[i];
-    return result;
+    return (lhs.w * rhs.w + lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z);
   }
 
-  template<typename T>
-  inline T cross(const Quat<T>& lhs, const Quat<T>& rhs)
+  inline Quat cross(const Quat& lhs, const Quat& rhs)
   {
-		return Quat<T>(
+		return Quat(
 			lhs.w * rhs.w - lhs.x * rhs.x - lhs.y * rhs.y - lhs.z * rhs.z,
 			lhs.w * rhs.x + lhs.x * rhs.w + lhs.y * rhs.z - lhs.z * rhs.y,
 			lhs.w * rhs.y + lhs.y * rhs.w + lhs.z * rhs.x - lhs.x * rhs.z,
-			lhs.w * rhs.z + lhs.z * rhs.w + lhs.x * rhs.y - lhs.y * rhs.x);
+			lhs.w * rhs.z + lhs.z * rhs.w + lhs.x * rhs.y - lhs.y * rhs.x
+    );
   }
 
-  template<typename T>
-  inline Quat<T> conjugate(const Quat<T>& rhs)
+  inline Quat conjugate(const Quat& rhs)
   {
     return Quat(
       rhs.w,
@@ -280,55 +253,49 @@ template<typename T>
     );
   }
 
-  template<typename T>
-  inline Quat<T> inverse(const Quat<T>& rhs)
+  inline Quat inverse(const Quat& rhs)
   {
     return (conjugate(rhs) / length(rhs));
   }
 
   // NOTE: quaternion must be normalized
-  template<typename T>
-  inline Vec<3, T> rotate(const Vec<3, T>& v, const Quat<T>& q)
+  inline Vec<3, float> rotate(const Vec<3, float>& v, const Quat& q)
   {
-    Quat<T> out = q * Quat<T>(v) * q.conj();
-    return out.real;
+    return (q * Quat(v) * conjugate(q)).real;
   }
 
-  // template<typename T>
-  // inline Quat<T> look_at(const Vec<3, T>& eye, const Vec<3, T>& target, const Vec<3, T>& up)
+  // // inline Quat look_at(const Vec<3, float>& eye, const Vec<3, float>& target, const Vec<3, float>& up)
   // {
-  //   Vec<3, T> f = normalize(target - eye); // forward vector
-  //   Vec<3, T> r = normalize(cross(up, f)); // right axis
-  //   Vec<3, T> u = cross(f, r); // rotated up
+  //   Vec<3, float> f = normalize(target - eye); // forward vector
+  //   Vec<3, float> r = normalize(cross(up, f)); // right axis
+  //   Vec<3, float> u = cross(f, r); // rotated up
 
-  //   Mat<3, 3, T> rot({
+  //   Mat<3, 3, float> rot({
   //     {r.x, u.x, f.x},
   //     {r.y, u.y, f.y},
   //     {r.z, u.z, f.z}
   //   });
   //   // TODO: use Mat3x3
   //   // return Mat4::to_quat(rot);
-  //   return Quat<T>::identity;
+  //   return Quat::identity;
   // }
 
-  // template<typename T>
-  // inline Quat<T> look_at(const Vec<3, T>& eye, const Vec<3, T>& target)
+  // // inline Quat look_at(const Vec<3, float>& eye, const Vec<3, float>& target)
   // {
-  //   return look_at(eye, target, Vec<3, T>::unit_y);
+  //   return look_at(eye, target, Vec<3, float>::unit_y);
   // }
 
   // NOTE: quaternion must be normalized
-  template<typename T>
-  inline Quat<T> slerp(const Quat<T>& q1, const Quat<T>& q2, T t)
+  inline Quat slerp(const Quat& q1, const Quat& q2, float t)
   {
-    T cos_th_2 = dot(q1, q2);
+    float cos_th_2 = dot(q1, q2);
     int s = (cos_th_2 < 0.0f) ? -1 : 1; // shortest path from acos
     cos_th_2 *= s;
 
-    T th_2 = acos(cos_th_2);
-    T sin_th_2 = sqrt(1.0f - cos_th_2 * cos_th_2);
+    float th_2 = acos(cos_th_2);
+    float sin_th_2 = sqrt(1.0f - cos_th_2 * cos_th_2);
     
-    T a, b;
+    float a, b;
     // theta approaches 180 degrees
     if (abs(sin_th_2) < 0.001f)
     {
@@ -344,49 +311,42 @@ template<typename T>
     return ((a * q1) + ((s * b) * q2));
   }
 
-  template<typename T>
-  inline Quat<T> slerp_clamp(const Quat<T>& q1, const Quat<T>& q2, T t)
-  {
-    T tc = clamp(t, 0.0f, 1.0f);
-    return slerp(q1, q2, tc);
-  }
+  // inline Quat slerp_clamp(const Quat& q1, const Quat& q2, float t)
+  // {
+  //   float tc = clamp(t, 0.0f, 1.0f);
+  //   return slerp(q1, q2, tc);
+  // }
 
-  template<typename T>
-  inline Quat<T> nlerp(const Quat<T>& q1, const Quat<T>& q2, T t)
+  inline Quat nlerp(const Quat& q1, const Quat& q2, float t)
   {
-    T cos_th_2 = dot(q1, q2);
+    float cos_th_2 = dot(q1, q2);
     int s = (cos_th_2 < 0.0f) ? -1 : 1; // shortest path from acos
     return normalize((q1 * (1.0f - t)) + (q2 * (s * t)));
   }
 
-  template<typename T>
-  inline Quat<T> nlerp_clamp(const Quat<T>& q1, const Quat<T>& q2, T t)
-  {
-    T tc = clamp(t, 0.0f, 1.0f);
-    return nlerp(q1, q2, tc);
-  }
+  // inline Quat nlerp_clamp(const Quat& q1, const Quat& q2, float t)
+  // {
+  //   float tc = clamp(t, 0.0f, 1.0f);
+  //   return nlerp(q1, q2, tc);
+  // }
 
-  template<typename T>
-  inline Quat<T> squad(const Quat<T>& q1, const Quat<T>& q2, const Quat<T>& s1, const Quat<T>& s2, T t)
+  inline Quat squad(const Quat& q1, const Quat& q2, const Quat& s1, const Quat& s2, float t)
   {
     return slerp(slerp(q1, q2, t), slerp(s1, s2, t), 2 * t * (1 - t));
   }
 
-  template<typename T>
-  inline Quat<T> squad_clamp(const Quat<T>& q1, const Quat<T>& q2, const Quat<T>& s1, const Quat<T>& s2, T t)
-  {
-    T tc = clamp(t, 0.0f, 1.0f);
-    return squad(q1, q2, s1, s2, tc);
-  }
+  // inline Quat squad_clamp(const Quat& q1, const Quat& q2, const Quat& s1, const Quat& s2, float t)
+  // {
+  //   float tc = clamp(t, 0.0f, 1.0f);
+  //   return squad(q1, q2, s1, s2, tc);
+  // }
 
-  // template<typename T>
-  // inline Quat<T> spline(const Quat<T> seq[], int sz, T t)
+  // // inline Quat spline(const Quat seq[], int sz, float t)
   // {
 
   // }
 
-  // template<typename T>
-  // inline Quat<T> spline_clamp(const Quat<T> seq[], int sz, T t)
+  // // inline Quat spline_clamp(const Quat seq[], int sz, float t)
   // {
 
   // }
