374745e168ba9a38af18dfcfd8531056e2f12ed1
Johann von Tiesenhausen <blah424@gmail.com>
2021-09-08 20:48:08 -0400

    fixed sprintf type-safety

--- src/ndv/str_helper.h
+++ src/ndv/str_helper.h
@@ -9,7 +9,7 @@ namespace ndv
   {
     int size = std::snprintf(nullptr, 0, fmt.c_str(), std::forward<Args>(args)...);
     std::string output(size + 1, '\0');
-    std::sprintf(&output[0], fmt.c_str(), std::forward<Args>(args)...);
+    std::snprintf(&output[0], sizeof(output), fmt.c_str(), std::forward<Args>(args)...);
     return output;
   }
 }
